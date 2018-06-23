839cc12e947a78e1a0c7e186fa53291b4a52fab2
加文雄 <jiawenxiong@newbeescm.com>
2018-06-23 15:45:52 +0800

    创建maven学习工程

---
+++ mvn-module-a/src/main/java/com/architect/mvn/service/HelloMaven.java
@@ -0,0 +1,10 @@
+package com.architect.mvn.service;
+
+import com.architect.mvn.common.utils.SoutUtil;
+
+public class HelloMaven {
+    public static void main(String[] args) {
+        String str = "Hello Maven a";
+        SoutUtil.print(str);
+    }
+}
